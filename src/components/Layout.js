a387cb7fffd66e5ea048989f3e5feae89384c855
jeffrey-t-l <jeffrey.leibensperger@gmail.com>
2021-03-02 09:12:02 -0500

    upgrading to GatsbyJS v3

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,9 @@
 import React from "react"
 import SEO from "./SEO"
 import { Link } from "gatsby"
+import DarkskyQuery from "../components/DarkskyQuery"
+
+const DarkskyWeatherQuery = DarkskyQuery()
 
 const Layout = ({ children }) => {
   return (
