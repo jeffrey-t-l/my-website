a7a2ba865758a36f14e8387feae26e65d6045648
jeffrey-t-l <jeffrey.leibensperger@gmail.com>
2021-03-07 20:25:22 -0500

    k

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,11 +11,11 @@ const Layout = ({ children }) => {
       <SEO />
       <div className="flex flex-col min-h-screen bg-gray-200">
         <nav class="flex items-center justify-between flex-wrap p-6 bg-black text-white font-semibold">
-          <div class="flex items-center flex-no-shrink text-white mr-6">
+          <div class="flex items-left flex-no-shrink text-white mr-6">
             {/*<svg class="h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>*/}
             <Link to="/"><span class="font-semibold text-xl tracking-tight">Jeff Leibensperger</span></Link>
           </div>
-          <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div class="w-full block flex-grow lg:flex lg:items-left lg:w-auto">
             <div class="text-sm lg:flex-grow">
               <Link to="/books">
                 <span class="p-4 bg-black text-white text-s rounded shadow hover:bg-gray-700">
