380b87fc4eb5e66dd9592e3aaa22eee59379d849
jeffrey-t-l <jeffrey.leibensperger@gmail.com>
2021-03-07 20:04:52 -0500

    formatting

--- src/pages/books.js
+++ src/pages/books.js
@@ -8,21 +8,21 @@ const Books = ({ children }) => {
             <div>
                 <div class="grid grid-rows-1 md:grid-rows-1 ...">
                     <p className="px-2 mt-4 text-lg text-gray-700 sm:mt-8"><strong>Books I am currently reading</strong></p>
-                    <div className="px-2 mt-8 text-sm text-gray-700">That War of Art; Steven Pressfield</div>
+                    <div className="px-6 mt-8 text-sm text-gray-700">That War of Art; Steven Pressfield</div>
                 </div>
                 <div class="grid grid-rows-1 md:grid-rows-1 ...">
-                    <p className="px-4 mt-8 text-lg text-gray-700 sm:mt-8"><strong>Books I would reccomend</strong></p>
-                    <div className="px-2 mt-8 text-sm text-gray-700">And the Hippos Were Boiled in their Tanks; William S Burroughs &amp; Jack Keroauc</div>
-                    <div className="px-2 mt-8 text-sm text-gray-700">Slaughterhouse Five; Kurt Vonnegut</div>
-                    <div className="px-2 mt-8 text-sm text-gray-700">Cat's Cradle; Kurt Vonnegut</div>
-                    <div className="px-2 mt-8 text-sm text-gray-700">American Buffalo; Steven Rinella</div>
-                    <div className="px-2 mt-8 text-sm text-gray-700">Animal Farm, George Orwell</div>
-                    <div className="px-2 mt-8 text-sm text-gray-700">On The Road, Jack Keroauc</div>
-                    <div className="px-2 mt-8 text-sm text-gray-700">Island; Aldous Huxley</div>
+                    <p className="px-2 mt-8 text-lg text-gray-700 sm:mt-8"><strong>Books I call my favorites</strong></p>
+                    <div className="px-6 mt-8 text-sm text-gray-700">And the Hippos Were Boiled in their Tanks; William S Burroughs &amp; Jack Keroauc</div>
+                    <div className="px-6 mt-8 text-sm text-gray-700">Slaughterhouse Five; Kurt Vonnegut</div>
+                    <div className="px-6 mt-8 text-sm text-gray-700">Cat's Cradle; Kurt Vonnegut</div>
+                    <div className="px-6 mt-8 text-sm text-gray-700">American Buffalo; Steven Rinella</div>
+                    <div className="px-6 mt-8 text-sm text-gray-700">Animal Farm, George Orwell</div>
+                    <div className="px-6 mt-8 text-sm text-gray-700">On The Road, Jack Keroauc</div>
+                    <div className="px-6 mt-8 text-sm text-gray-700">Island; Aldous Huxley</div>
                 </div>
                 <div class="grid grid-rows-1 md:grid-rows-1 ...">
-                    <p className="px-4 mt-8 text-lg text-gray-700 sm:mt-8"><strong>Books I'd like to read next</strong></p>
-                    <div className="px-2 mt-8 text-sm text-gray-700">A World Without Email; Cal Newport</div>
+                    <p className="px-2 mt-8 text-lg text-gray-700 sm:mt-8"><strong>Books I'd like to read next</strong></p>
+                    <div className="px-6 mt-8 text-sm text-gray-700">A World Without Email; Cal Newport</div>
                 </div>
             </div>
         </main>
