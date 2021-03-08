1875818a2c3be14b826fdf28a2cda4143dcb624a
jeffrey-t-l <jeffrey.leibensperger@gmail.com>
2021-03-07 20:47:19 -0500

    fixes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,33 +10,33 @@ const Layout = ({ children }) => {
     <>
       <SEO />
       <div className="flex flex-col min-h-screen bg-gray-200">
-        <nav class="flex items-center justify-between flex-wrap p-6 bg-black text-white font-semibold">
-          <div class="flex items-left flex-no-shrink text-white mr-6">
-            {/*<svg class="h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>*/}
-            <Link to="/"><span class="font-semibold text-xl tracking-tight">Jeff Leibensperger</span></Link>
+        <nav className="flex items-center justify-between flex-wrap p-6 bg-black text-white font-semibold">
+          <div className="flex items-left flex-no-shrink text-white mr-6">
+            {/*<svg className="h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>*/}
+            <Link to="/"><span className="font-semibold text-xl tracking-tight">Jeff Leibensperger</span></Link>
           </div>
-          <div class="w-full block flex-grow lg:flex lg:items-left lg:w-auto">
-            <div class="text-sm lg:flex-grow">
+          <div className="w-full block flex-grow lg:flex lg:items-left lg:w-auto">
+            <div className="text-sm lg:flex-grow">
               <Link to="/books">
-                <span class="p-4 bg-black text-white text-s rounded shadow hover:bg-gray-700">
+                <span className="p-4 bg-black text-white text-s rounded shadow hover:bg-gray-700">
                   Good Reads
                 </span>
               </Link>
               <Link to="/resume">
-                <span class="p-4 bg-black text-white text-s rounded shadow hover:bg-gray-700">
+                <span className="p-4 bg-black text-white text-s rounded shadow hover:bg-gray-700">
                   Work Experience
                 </span>
               </Link>
             </div>
           </div>
-          <div class="w-full block flex-grow lg:flex lg:items-right lg:w-auto">
-            <div  class="text-sm">
+          <div className="w-full block flex-grow lg:flex lg:items-right lg:w-auto">
+            <div  className="text-sm">
               <a href="https://github.com/jeffrey-t-l/" target="_blank" rel="noreferrer"
                     className="p-4 bg-black text-white text-xs rounded shadow hover:bg-gray-700">
                     GitHub
               </a>
             </div>
-            <div  class="text-sm">
+            <div  className="text-sm">
               <a href="https://www.linkedin.com/in/jeffleibensperger/" target="_blank" rel="noreferrer"
                     className="p-4 bg-black text-white text-xs rounded shadow hover:bg-gray-700">
                     LinkedIn
@@ -48,10 +48,10 @@ const Layout = ({ children }) => {
         <footer className="p-16 bg-black text-gray-600 text-xs text-right align-bottom">
           <div><a href="https://github.com/melanienolan/gatsby-starter-tailwind-css" target="_blank" rel="noreferrer">Website Template</a> | Icons by <a href="https://www.freepik.com"  target="_blank" rel="noreferrer" title="Freepik">Freepik</a> | Weather from <a href="https://darksky.net"  target="_blank" rel="noreferrer" title="Darsky">Darksky</a></div>
         </footer>
-        {/*<footer class="w-full border-t border-black p-8 pin-b bg-black text-gray-600">
-          <div class="h-48 flex flex-wrap content-center">
+        {/*<footer className="w-full border-t border-black p-8 pin-b bg-black text-gray-600">
+          <div className="h-48 flex flex-wrap content-center">
             <div>1</div>
-            <div class="text-xs text-right text-bottom">Template by <a href="https://github.com/melanienolan/gatsby-starter-tailwind-css" target="_blank" rel="noreferrer">Melanie Nolan</a> | Icons by <a href="https://www.freepik.com"  target="_blank" rel="noreferrer" title="Freepik">Freepik</a> | Weather from <a href="https://darksky.net"  target="_blank" rel="noreferrer" title="Darsky">Darksky</a></div>
+            <div className="text-xs text-right text-bottom">Template by <a href="https://github.com/melanienolan/gatsby-starter-tailwind-css" target="_blank" rel="noreferrer">Melanie Nolan</a> | Icons by <a href="https://www.freepik.com"  target="_blank" rel="noreferrer" title="Freepik">Freepik</a> | Weather from <a href="https://darksky.net"  target="_blank" rel="noreferrer" title="Darsky">Darksky</a></div>
           </div>
         </footer>*/}
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,9 @@ import React from "react"
 import Layout from "../components/Layout"
 import Emoji from "../components/Emoji"
 import heroImage from "../images/ws-me-point-reyes-2017.jpg"
-//import { StaticQuery, graphql } from "gatsby"
-//import Darksky from "../components/Darksky"
-import DarkskyQuery from "../components/DarkskyQuery"
+//import DarkskyQuery from "../components/DarkskyQuery"
 
-//const DarkskyWeather = Darksky()
-const DarkskyWeatherQuery = DarkskyQuery()
+//const DarkskyWeatherQuery = DarkskyQuery()
 
 const Index = () => {
   return (
@@ -20,11 +17,13 @@ const Index = () => {
                 Hi There! <Emoji symbol="👋" label="wave"/>
               </h1>
               <p className="px-4 mt-8 text-lg text-gray-700 sm:mt-8">
-                Some placeholder text here. Yada yada. Will update later.
+                I am Jeff. I currently reside in <a href="https://goo.gl/maps/eVYSz1HcDd2iFE2E7" target="_blank" rel="noreferrer">Astoria, NY.</a>
               </p>
+              {/*}
               <span>
-              <p className="px-4 mt-8 text-lg text-gray-700 sm:mt-8">
-                The current temperature in <strong><a href="https://goo.gl/maps/eVYSz1HcDd2iFE2E7" target="_blank" rel="noreferrer">Astoria, NY</a></strong> is currently {DarkskyWeatherQuery} &deg; F.</p></span>
+                <p className="px-4 mt-8 text-lg text-gray-700 sm:mt-8">The current temperature in <strong><a href="https://goo.gl/maps/eVYSz1HcDd2iFE2E7" target="_blank" rel="noreferrer">Astoria, NY</a></strong> is currently {DarkskyWeatherQuery} &deg; F.</p>
+              </span>
+              */}
               <br /><br />
             </div>
           </div>
@@ -34,15 +33,6 @@ const Index = () => {
             alt="Pt. Reyes National Seashore"
           ></img>
         </div>
-
-        {/*
-        <div className="flex items-center p-3 mx-2 bg-white rounded shadow-xs sm:mx-auto">
-          <code className="text-gray-600 text-xs leading-xl">
-            gatsby new my-project-name
-            https://github.com/melanienolan/gatsby-starter-tailwind-css
-          </code>
-        </div>
-        */}
       </main>
     </Layout>
   )
